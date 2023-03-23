6077369ad2afae9066114337e2969a986ebd3e8b
Vitalii Smirnov <vigsmirnov@sberbank.ru>
2023-03-23 14:52:49 +0300

    qf

--- tgbot.py
+++ tgbot.py
@@ -109,7 +109,7 @@ def chat_with_gpt(update: Update, context: CallbackContext) -> None:
     openai_response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=(f"{training_prompts}\n{history}"),
-        max_tokens=150,
+        max_tokens=512,
         n=1,
         stop=None,
         temperature=0.5,

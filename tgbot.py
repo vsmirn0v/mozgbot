959a5e277777bdf9e1555597e91e51c62867310d
Vitalii Smirnov <vigsmirnov@sberbank.ru>
2023-03-27 21:46:46 +0300

    qf

--- tgbot.py
+++ tgbot.py
@@ -195,7 +195,7 @@ def chat_with_gpt(update: Update, context: CallbackContext) -> None:
                         break
                 #logging.info(f"HSTT: {conversation_history_truncated}")
                 logging.info(f"MTOKENS: {max_tokens} TOKENS: {num_tokens_from_list(training_prompts + conversation_history_truncated)}")
-                openai_params["messages"] = training_prompts + reversed(conversation_history_truncated)
+                openai_params["messages"] = training_prompts + conversation_history_truncated.reverse()
                 openai_response = openai.ChatCompletion.create(**openai_params)
                 history = conversation_history_truncated
             else:
